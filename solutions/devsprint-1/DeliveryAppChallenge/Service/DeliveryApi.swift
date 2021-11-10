be6130cbf76df8895cb355ebb49beea925a0af93
joao <joao.camargo@gmail.com>
2021-11-09 23:02:13 -0300

    implementacoa search addresses

--- solutions/devsprint-1/DeliveryAppChallenge/Service/DeliveryApi.swift
+++ solutions/devsprint-1/DeliveryAppChallenge/Service/DeliveryApi.swift
@@ -78,4 +78,42 @@ extension DeliveryApi: DeliveryApiProtocol {
         }
         task.resume()
     }
+    
+    func searchAddresses(_ completion: @escaping (Result<[Address], DeliveryApiError>) -> Void) {
+        guard let url = URL(string: "https://raw.githubusercontent.com/devpass-tech/challenge-delivery-app/main/api/address_search_results.json") else {
+            return completion(.failure(.invalidURL))
+        }
+        let request = URLRequest(url: url)
+
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+
+        let task = session.dataTask(with: request) { data, response, error in
+            if let _ = error {
+                return completion(.failure(.serverError))
+            }
+
+            guard let response = response as? HTTPURLResponse else {
+                return completion(.failure(.responseError))
+            }
+
+            guard (200...299).contains(response.statusCode) else {
+                return completion(.failure(.networkError(response.statusCode)))
+            }
+
+            guard let data = data else {
+                return completion(.failure(.decodificationError))
+            }
+
+            guard let addresses: [Address] = data.jSONDecode(using: .convertFromSnakeCase) else {
+                return completion(.failure(.decodificationError))
+            }
+
+            DispatchQueue.main.async {
+                completion(.success(addresses))
+            }
+        }
+        task.resume()
+    }
+    
 }

4030057859d61d67a18445c52c29d425cd7945c1
All3yp <noyallepereira@gmail.com>
2022-01-25 20:04:45 -0300

    Criação da: APIService com função get generica, camada de ServiceError e Router para implementação das rotas

---
+++ solutions/devsprint-vinicius-carvalho-1/DeliveryAppChallengeTests/DeliveryApiTests.swift
@@ -0,0 +1,8 @@
+//
+//  DeliveryApiTests.swift
+//  DeliveryAppChallengeTests
+//
+//  Created by Alley Pereira on 25/01/22.
+//
+
+import Foundation
---
+++ solutions/devsprint-vinicius-carvalho-1/DeliveryAppChallengeTests/Mocks/URLProtocolMock.swift
@@ -0,0 +1,8 @@
+//
+//  URLProtocolMock.swift
+//  DeliveryAppChallengeTests
+//
+//  Created by Alley Pereira on 25/01/22.
+//
+
+import Foundation

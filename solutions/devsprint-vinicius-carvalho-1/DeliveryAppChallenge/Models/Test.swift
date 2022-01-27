b72d64f3c0d18f70eef7562cd6c12bb9645b55c8
lysonjeada <contatotechlys@gmail.com>
2022-01-26 21:15:43 -0300

    Add Address Model

---
+++ solutions/devsprint-vinicius-carvalho-1/DeliveryAppChallenge/Models/Test.swift
@@ -0,0 +1,17 @@
+//
+//  Test.swift
+//  DeliveryAppChallenge
+//
+//  Created by Amaryllis Baldrez on 26/01/22.
+//
+
+import Foundation
+
+struct Address: Decodable {
+    private let street: String
+    private let number: String
+    private let neighborhood: String
+    
+    
+}
+

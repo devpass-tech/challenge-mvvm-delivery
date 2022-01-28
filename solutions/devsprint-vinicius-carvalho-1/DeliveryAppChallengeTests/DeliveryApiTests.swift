7b09d6709cdbb56ddb9bbc9748ff2b0e7ea58c9f
All3yp <noyallepereira@gmail.com>
2022-01-27 22:32:29 -0300

    Criando restaurantDetails request com testes unitários

--- solutions/devsprint-vinicius-carvalho-1/DeliveryAppChallengeTests/DeliveryApiTests.swift
+++ solutions/devsprint-vinicius-carvalho-1/DeliveryAppChallengeTests/DeliveryApiTests.swift
@@ -16,16 +16,19 @@ class DeliveryApiTests: XCTestCase {
 
 	class APIServiceSpy: APIServiceProtocol {
 
-		var session: URLSession = {
-			URLProtocolMock.mockSession(
-				with: Router.fetchRestaurants.getRequest,
-				completionMock: (nil,nil,nil)
-			)
-		}()
+		let session: URLSession
+
+		init(session: URLSession) {
+			self.session = session
+		}
 
 		var getCalled: Bool = false
 
-		func get<T>(request: URLRequest, of type: T.Type, completion: @escaping (Result<T, ServiceError>) -> Void) where T : Decodable {
+		func get<T>(
+			request: URLRequest,
+			of type: T.Type,
+			completion: @escaping (Result<T, ServiceError>) -> Void
+		) where T : Decodable {
 			getCalled = true
 		}
 	}
@@ -43,7 +46,12 @@ class DeliveryApiTests: XCTestCase {
 
 	func test_fetchRestaurants_apiManagerCallsGet() {
 		// Given
-		let spy = APIServiceSpy()
+		let spy = APIServiceSpy(
+			session: URLProtocolMock.mockSession(
+				with: Router.fetchRestaurants.getRequest,
+				completionMock: (nil,nil,nil)
+			)
+		)
 		self.sut.serviceManager = spy
 
 		// When
@@ -54,8 +62,110 @@ class DeliveryApiTests: XCTestCase {
 		}
 	}
 
-	private func encode(_ dataMock: [RestaurantsListModel]) -> Data {
-		try! JSONEncoder().encode(dataMock)
+	func test_fetchRestaurantList_apiManagerSendsNilForRequestEmpty() {
+		// Given
+		let spy = APIServiceSpy(
+			session: URLProtocolMock.mockSession(
+				with: Router.fetchRestaurantDetails.getRequest,
+				completionMock: (nil,nil,ServiceError.emptyData)
+			)
+		)
+		self.sut.serviceManager = spy
+
+		// When
+		sut.fetchRestaurants { list in
+
+			// Then
+			XCTAssert(list.isEmpty)
+		}
 	}
 
+	func test_fetchRestaurantList_aapiManagerReturnNonEmptyList() {
+		// Given
+		let listMock = [RestaurantsListModel(
+			name: "Benjamin a Padaria",
+			category: "Padaria",
+			deliveryTime: .init(min: 10, max: 45)
+		)]
+		let data: Data = try! JSONEncoder().encode(listMock)
+		let spy = APIServiceSpy(
+			session:
+				URLProtocolMock.mockSession(
+					with: Router.fetchRestaurants.getRequest,
+					completionMock: (data, nil, nil)
+				)
+		)
+		self.sut.serviceManager = spy
+
+		// When
+		sut.fetchRestaurants { listArray in
+			// Then
+			listArray.enumerated().forEach { (index, restaurant) in
+				XCTAssertEqual(restaurant.category, listMock[index].category)
+			}
+		}
+	}
+
+	func test_fetchRestaurantDetails_apiManagerCallsGet() {
+		// Given
+		let spy = APIServiceSpy(
+			session: URLProtocolMock.mockSession(
+				with: Router.fetchRestaurantDetails.getRequest,
+				completionMock: (nil,nil,nil)
+			)
+		)
+		self.sut.serviceManager = spy
+
+		// When
+		sut.fetchRestaurantDetails { _ in
+
+		// Then
+			XCTAssertTrue(spy.getCalled)
+
+		}
+	}
+
+	func test_fetchRestaurantDetails_apiManagerSendsNilForRequestError() {
+		// Given
+		let spy = APIServiceSpy(
+			session: URLProtocolMock.mockSession(
+				with: Router.fetchRestaurantDetails.getRequest,
+				completionMock: (nil,nil,ServiceError.decodeError)
+			)
+		)
+		self.sut.serviceManager = spy
+
+		// When
+		sut.fetchRestaurantDetails { details in
+
+			// Then
+			XCTAssertNil(details)
+		}
+	}
+
+	func test_fetchRestaurantDetails_apiManagerReturnNonEmptyDetails() {
+		// Given
+		let detailsMock = RestaurantDetailsModel(
+			name: "Benjamin a Padaria",
+			category: "Padaria",
+			deliveryTime: .init(min: 10, max: 45),
+			reviews: .init(score: 4.6, count: 100),
+			menu: .init(name: "Bolo de Laranja 🍊", category: "Padaria", price: 9.99)
+		)
+
+		let data: Data = try! JSONEncoder().encode(detailsMock)
+		let spy = APIServiceSpy(
+			session: URLProtocolMock.mockSession(
+				with: Router.fetchRestaurantDetails.getRequest,
+				completionMock: (data, nil, nil)
+			)
+		)
+		self.sut.serviceManager = spy
+
+		// When
+		sut.fetchRestaurantDetails { details in
+			// Then
+			XCTAssertEqual(details?.name, detailsMock.name)
+		}
+	}
 }

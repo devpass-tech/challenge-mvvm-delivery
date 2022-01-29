096eb1e86f5cf355eb8676a2d3251e0f0ce53517
Murillo R. Araújo <mr_araujo@icloud.com>
2022-01-29 12:23:25 -0300

    feat(mvvm): Add Restaurant Details ViewModel

---
+++ solutions/devsprint-vinicius-carvalho-1/DeliveryAppChallenge/Screens/RestaurantDetails/RestaurantDetailsViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  RestaurantDetailsViewModel.swift
+//  DeliveryAppChallenge
+//
+//  Created by Murillo R. Araújo on 29/01/22.
+//
+
+import Foundation
+
+protocol RestaurantDetailsPresentable: AnyObject {
+    func displayRestaurantDetails(with restaurant: Restaurant)
+    func displayErros(error: Error)
+}
+
+class RestaurantDetailsViewModel {
+    let service: APIServiceProtocol
+
+    weak var presenter: RestaurantDetailsPresentable?
+
+    init(with service: APIServiceProtocol = APIService()) {
+        self.service = service
+    }
+
+    func loadRestaurantDetails() {
+
+        service.get(request: Router.fetchRestaurantDetails.getRequest, of: Restaurant.self ) { (result: Result<Restaurant, ServiceError>) in
+            switch result {
+            case .success(let restaurant):
+                self.presenter?.displayRestaurantDetails(with: restaurant)
+
+            case .failure(let error):
+                self.presenter?.displayErros(error: error)
+            }
+        }
+    }
+}

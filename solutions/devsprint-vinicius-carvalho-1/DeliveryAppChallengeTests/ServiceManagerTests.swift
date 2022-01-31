e32bd07c6753d92957e37b8e4fcdf7cc1493353c
Rodrigo Lemos <rodlemos.d@gmail.com>
2022-01-31 16:03:03 -0400

    Create address model, search address integration and unit test

--- solutions/devsprint-vinicius-carvalho-1/DeliveryAppChallengeTests/ServiceManagerTests.swift
+++ solutions/devsprint-vinicius-carvalho-1/DeliveryAppChallengeTests/ServiceManagerTests.swift
@@ -176,6 +176,111 @@ class ServiceManagerTests: XCTestCase {
 
 		wait(for: [expectation], timeout: 1)
 	}
+    
+    func test_get_whenFetchAddress_shouldReturnSuccess() {
+        // Given
+        let endpoint = Router.fetchAddress
+        let addressDataMock = addressDataMock()
+        let successCompletion = createResquestCompletionMock(endpoint: endpoint,
+                                                             dataMock: addressDataMock,
+                                                             statusCode: 200,
+                                                             error: nil)
+        
+        let mockSession = URLProtocolMock.mockSession(with: endpoint.getRequest, completionMock: successCompletion)
+        self.sut = APIService(mockSession)
+        let expectation = expectation(description: "Success")
+        
+        // When
+        sut.get(request: endpoint.getRequest, of: [Address].self) { result in
+            // Then
+            switch result {
+            case .failure:
+                XCTFail()
+            case .success(let address):
+                expectation.fulfill()
+                XCTAssertFalse(address.isEmpty)
+                if let address = try? XCTUnwrap(address.first) {
+                    XCTAssertEqual(address.street, addressDataMock[0].street)
+                    XCTAssertEqual(address.number, addressDataMock[0].number)
+                    XCTAssertEqual(address.neighborhood, addressDataMock[0].neighborhood)
+                } else {
+                    XCTFail()
+                }
+            }
+        }
+        wait(for: [expectation], timeout: 0.7)
+    }
+    
+    func test_get_whenFetchAddressEmptyData_shouldReturnFailure() {
+        // Given
+        let endpoint = Router.fetchAddress
+        let successCompletion: URLProtocolMock.RequestCompletion = (nil,nil,nil)
+        let mockSession = URLProtocolMock.mockSession(with: endpoint.getRequest, completionMock: successCompletion)
+        self.sut = APIService(mockSession)
+
+        let expectation = expectation(description: "failure")
+
+        // When
+        sut.get(request: endpoint.getRequest, of: [Address].self) { result in
+            // Then
+            switch result {
+            case .failure(let error):
+                expectation.fulfill()
+                XCTAssertEqual(error.localizedDescription, ServiceError.emptyData.localizedDescription)
+            case .success:
+                XCTFail()
+            }
+        }
+        wait(for: [expectation], timeout: 0.7)
+    }
+    
+    func test_whenErrorFetchAddress_shouldReturnFailure() {
+        // Given
+        let endpoint = Router.fetchAddress
+        let expectedError = NSError(domain: NSURLErrorDomain, code: NSURLErrorTimedOut, userInfo: nil)
+        let successCompletion: URLProtocolMock.RequestCompletion = (nil,nil,expectedError)
+        let mockSession = URLProtocolMock.mockSession(with: endpoint.getRequest, completionMock: successCompletion)
+        self.sut = APIService(mockSession)
+
+        let expectation = expectation(description: "failure")
+
+        // When
+        sut.get(request: endpoint.getRequest, of: [Address].self) { result in
+            // Then
+            switch result {
+            case .failure(let error):
+                expectation.fulfill()
+                XCTAssertEqual(error.localizedDescription,
+                               ServiceError.requestFailed(description: expectedError.localizedDescription).localizedDescription)
+            case .success:
+                XCTFail()
+            }
+        }
+        wait(for: [expectation], timeout: 0.7)
+    }
+    
+    func test_get_whenAddressDecodeError_shouldReturnFailure() {
+        // Given
+        let endpoint = Router.fetchAddress
+        let successCompletion: URLProtocolMock.RequestCompletion = ("{\"key\": \"value\"}".data(using: .utf8), nil, nil)
+        let mockSession = URLProtocolMock.mockSession(with: endpoint.getRequest, completionMock: successCompletion)
+        self.sut = APIService(mockSession)
+
+        let expectation = expectation(description: "failure")
+
+        // When
+        sut.get(request: endpoint.getRequest, of: [Address].self) { result in
+            // Then
+            switch result {
+            case .failure(let error):
+                expectation.fulfill()
+                XCTAssertEqual(error.localizedDescription, ServiceError.decodeError.localizedDescription)
+            case .success:
+                XCTFail()
+            }
+        }
+        wait(for: [expectation], timeout: 1)
+    }
 
 
 
@@ -206,5 +311,10 @@ class ServiceManagerTests: XCTestCase {
 		]
 		return dataMock
 	}
+    
+    private func addressDataMock() -> [Address] {
+        let addressDataMock: [Address] = [Address(street: "Rua Augusta", number: "495", neighborhood: "Consolação")]
+        return addressDataMock
+    }
 
 }

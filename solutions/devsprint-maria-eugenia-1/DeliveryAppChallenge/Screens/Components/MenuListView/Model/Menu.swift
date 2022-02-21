5eac173b4524bead5f7c322c7f3ad8083479dde7
rodrigofran <rodrigofrancms@gmail.com>
2022-02-21 19:28:44 -0300

    integraçao apiMenu com DeliveryAPI

---
+++ solutions/devsprint-maria-eugenia-1/DeliveryAppChallenge/Screens/Components/MenuListView/Model/Menu.swift
@@ -0,0 +1,14 @@
+//
+//  Menu.swift
+//  DeliveryAppChallenge
+//
+//  Created by Rodrigo Francischett Occhiuto on 21/02/22.
+//
+
+import Foundation
+
+struct Menu: Codable {
+    let category: String
+    let name: String
+    let price: Double
+}
---
+++ solutions/devsprint-maria-eugenia-1/DeliveryAppChallenge/Screens/Components/MenuListView/ViewModel/MenuViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MenuViewModel.swift
+//  DeliveryAppChallenge
+//
+//  Created by Rodrigo Francischett Occhiuto on 21/02/22.
+//
+
+import Foundation
+

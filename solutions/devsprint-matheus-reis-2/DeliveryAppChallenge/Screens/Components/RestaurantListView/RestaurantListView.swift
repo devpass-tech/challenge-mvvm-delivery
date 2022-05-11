c6f5e18a4d8e308358b50fcfdf151153f4b780af
Rodrigo Borges <rborges.soares@gmail.com>
2022-05-11 18:05:02 -0300

    Projeto base para Dev Sprint Matheus Reis #2

---
+++ solutions/devsprint-matheus-reis-2/DeliveryAppChallenge/Screens/Components/RestaurantListView/RestaurantListView.swift
@@ -0,0 +1,84 @@
+//
+//  RestaurantListView.swift
+//  DeliveryAppChallenge
+//
+//  Created by Rodrigo Borges on 31/10/21.
+//
+
+import UIKit
+
+class RestaurantListView: UIView {
+
+    static let cellSize = CGFloat(82)
+
+    private let cellIdentifier = "RestaurantCellIdentifier"
+
+    lazy var tableView: UITableView = {
+
+        let tableView = UITableView(frame: .zero)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(RestaurantCellView.self, forCellReuseIdentifier: self.cellIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+
+    init() {
+        super.init(frame: .zero)
+
+        backgroundColor = .white
+        addSubviews()
+        configureConstraints()
+
+        tableView.reloadData()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension RestaurantListView {
+
+    func addSubviews() {
+
+        addSubview(tableView)
+    }
+
+    func configureConstraints() {
+
+        NSLayoutConstraint.activate([
+
+            tableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+}
+
+extension RestaurantListView: UITableViewDataSource {
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 10
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantCellView
+
+        return cell
+    }
+}
+
+extension RestaurantListView: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return RestaurantListView.cellSize
+    }
+
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+}

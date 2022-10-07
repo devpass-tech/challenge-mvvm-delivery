9d331a4d37ace0a18063450dccdc6a14d43997b1
gabrielaragaoo <eng.gabrielaragao@gmail.com>
2022-10-07 15:15:28 -0300

    get stats devpass

--- solutions/devsprint-1/DeliveryAppChallenge/AppDelegate/AppDelegate.swift
+++ solutions/devsprint-1/DeliveryAppChallenge/AppDelegate/AppDelegate.swift
@@ -28,5 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    //
 }
 

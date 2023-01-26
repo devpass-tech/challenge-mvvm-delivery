39f20438ea70e52af1ae51714bc8a5d439d782a4
Bruno Nascimento Marques <mv.brunonmarques@gmail.com>
2023-01-26 18:08:39 -0300

    reviews model removed

--- solutions/devsprint-matheus-reis-6/DeliveryAppChallenge/Models/MenuItemDetails.swift
+++ solutions/devsprint-matheus-reis-6/DeliveryAppChallenge/Models/MenuItemDetails.swift
@@ -8,4 +8,5 @@
 import Foundation
 
 struct MenuItemDetails: Decodable {
+    
 }
--- solutions/devsprint-matheus-reis-6/DeliveryAppChallenge/Models/RestaurantDetails.swift
+++ solutions/devsprint-matheus-reis-6/DeliveryAppChallenge/Models/RestaurantDetails.swift
@@ -8,7 +8,5 @@
 import Foundation
 
 struct RestaurantDetails: Decodable {
-}
-
-struct Reviews: Decodable {
+    
 }

35535729b63bdffbcdd4b1f5063614438d54de0c
Bruno Nascimento Marques <mv.brunonmarques@gmail.com>
2023-01-27 17:38:21 -0300

    remove white line on home view controller

--- solutions/devsprint-matheus-reis-6/DeliveryAppChallenge/Screens/Home/HomeViewController.swift
+++ solutions/devsprint-matheus-reis-6/DeliveryAppChallenge/Screens/Home/HomeViewController.swift
@@ -24,7 +24,7 @@ class HomeViewController: UIViewController {
     }
 
     override func viewDidLoad() {
-        navigationController?.navigationBar.prefersLargeTitles = true 
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     override func loadView() {
